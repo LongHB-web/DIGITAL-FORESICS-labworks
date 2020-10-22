6eb84177e7938c9b8a33cd5a255ab60540546d02
LongHB-web <longhb.bi9147@st.usth.edu.vn>
2020-10-22 20:49:10 +0700

    update labwork1

--- labwork1/logo usth.py.py
+++ labwork1/logo usth.py.py
@@ -8,33 +8,10 @@ img = cv.imread(cv.samples.findFile("logo-usth.png"))
 if img is None:
     sys.exit("Could not read the image.")
 cv.imshow("Display window", img)
-import cv2
-
-distinct_colors = []
-a, b, c = cv2.split(img)
-distinct_imgs = []
-
-
-print("[] Set up colors")
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        if (a[i, j], b[i, j], c[i, j]) not in distinct_colors:
-            distinct_colors.append((a[i, j], b[i, j], c[i, j]))
-
-print("[] Appending copy...")
-for color in distinct_colors:
-    distinct_imgs.append(img.copy())
-
-print("[] Seperating...")
-for i in range(len(distinct_imgs)):
-    for j in range(len(img)):
-        for k in range(len(img[j])):
-            if img[j, k, 0] != distinct_colors[i][0] or img[j, k, 1] != distinct_colors[i][1] or img[j, k, 2] != distinct_colors[i][2]:
-                distinct_imgs[i][j, k] = [0, 0, 0]
-
-print("[] Write to file...")
-for i in range(len(distinct_imgs)):
-    cv2.imwrite("logo-distinct" + str(i) + ".png", distinct_imgs[i])
-
-
-k = cv.waitKey(0)
+b, g, r = cv.split(img)
+cv.imshow('image_blue', b)
+cv.waitKey(0)
+cv.imshow('image_green', g)
+cv.waitKey(0)
+cv.imshow('image_red', r)
+cv.waitKey(0)
