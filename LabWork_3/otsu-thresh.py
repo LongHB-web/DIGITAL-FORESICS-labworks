02b6aca629f34703c89f4be2ee95afa8f695e1e8
LongHB-web <longhb.bi9147@st.usth.edu.vn>
2020-11-05 14:57:31 +0700

    update lapwork3

---
+++ LabWork_3/otsu-thresh.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import sys
+
+forme1 = cv.imread(r'C:\Users\MY PC\Downloads\opencv\sources\samples\data\forme1.png',cv.IMREAD_GRAYSCALE)
+lena = cv.imread(r'C:\Users\MY PC\Downloads\opencv\sources\samples\data\lena.png',cv.IMREAD_GRAYSCALE)
+forme3 = cv.imread(r'C:\Users\MY PC\Downloads\opencv\sources\samples\data\forme3.png',cv.IMREAD_GRAYSCALE)
+
+ret, thresh_forme1 = cv.threshold(forme1, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+ret, thresh_lena = cv.threshold(lena, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+ret, thresh_forme3 = cv.threshold(forme3, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+
+cv.imshow("Otsu-Forme 1", thresh_forme1)
+cv.imshow("Otsu-Lena", thresh_lena)
+cv.imshow("Otsu-Forme 3", thresh_forme3)
+
+key = cv.waitKey(0)
